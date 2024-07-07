a0affc04724704383405103b0ef7027c8a7e8aaa
DayanaIngrid <dayanaingrid.040@gmail.com>
2024-07-07 00:20:26 -0300

    Alterações

--- api/config/settings.py
+++ api/config/settings.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-
 from pydantic.v1 import BaseSettings
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 
 class Settings(BaseSettings):
+    class Config:
+        env_file = f'{PROJECT_ROOT}/.env'
+        
     DATABASE_USER: str
     DATABASE_PASSWORD: str
     DATABASE_HOST: str
@@ -13,8 +15,14 @@ class Settings(BaseSettings):
     DATABASE_NAME: str
     DATABASE_TYPE: str
 
-    class Config:
-        env_file = f'{PROJECT_ROOT}/.env'
+    LOG_PATH: str
+    LOG_FILE_NAME: str
+
+    TOKEN_EXPIRE_SECONDS: int
+    TOKEN_SECRET_KEY: str
+
+    
 
 
 settings = Settings()
+settings.LOG_PATH = f'{PROJECT_ROOT}/{settings.LOG_PATH}'
