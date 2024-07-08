efa172df699c25f95c7552554e07d6ab2473d8d2
DayanaIngrid <dayanaingrid.040@gmail.com>
2024-07-08 11:24:11 -0300

    Correção

--- api/service/task_service.py
+++ api/service/task_service.py
@@ -2,6 +2,7 @@ import logging
 from fastapi import HTTPException
 from pydantic import TypeAdapter
 from sqlalchemy.exc import IntegrityError
+
 from api.domain.dto.dtos import TaskCreateDTO, TaskDTO, TaskUpdateDTO
 from api.domain.model.models import Task
 from api.repository.task_repository import TaskRepository
@@ -11,44 +12,44 @@ class TaskService:
     def __init__(self, task_repository: TaskRepository):
         self.task_repository = task_repository
 
-    def create(self, user_data: TaskCreateDTO) -> TaskDTO:
+    def create(self, task_data: TaskCreateDTO) -> TaskDTO:
         logging.info('Criando uma novo tarefa.')
-        user = Task(**user_data.model_dump())
+        task = Task(**task_data.model_dump())
         try:
-            created = self.task_repository.save(user)
+            created = self.task_repository.save(Task)
             return TypeAdapter(TaskDTO).validate_python(created)
         except IntegrityError as e:
-            logging.error(f'Erro ao criar o tarefa: {user_data.model_dump()}')
+            logging.error(f'Erro ao criar o tarefa: {task_data.model_dump()}')
             raise HTTPException(status_code=409, detail=f'Tarefa já existe na base: {e.args[0]}')
 
-    def read(self, user_id: int) -> TaskDTO:
+    def read(self, task_id: int) -> TaskDTO:
         logging.info('Buscando uma tarefa.')
-        return TypeAdapter(TaskDTO).validate_python(self._read(user_id))
+        return TypeAdapter(TaskDTO).validate_python(self._read(task_id))
 
-    def _read(self, user_id: int) -> Task:
-        user = self.task_repository.read(user_id)
-        if user is None:
-            logging.error(f'Tarefa {user_id} não encontrado.')
-            raise HTTPException(status_code=404, detail=f'Tarefa {user_id} não encontrado.')
-        return user
+    def _read(self, task_id: int) -> Task:
+        task = self.task_repository.read(task_id)
+        if task is None:
+            logging.error(f'Tarefa {task_id} não encontrado.')
+            raise HTTPException(status_code=404, detail=f'Tarefa {task_id} não encontrado.')
+        return task
 
     def find_all(self) -> list[TaskDTO]:
         logging.info('Buscando todos as tarefas.')
-        users = self.task_repository.find_all()
-        return [TypeAdapter(TaskDTO).validate_python(user) for user in users]
+        tasks = self.task_repository.find_all()
+        return [TypeAdapter(TaskDTO).validate_python(task) for task in tasks]
 
-    def update(self, user_id: int, user_data: TaskUpdateDTO):
-        logging.info(f'Atualizando a tarefa {user_id}.')
-        user = self._read(user_id)
-        user_data = user_data.model_dump(exclude_unset=True)
-        for key, value in user_data.items():
-            setattr(user, key, value)
-        user_updated = self.task_repository.save(user)
-        logging.info(f'Tarefa {user_id} atualizado: {user_updated}')
-        return TypeAdapter(TaskDTO).validate_python(user_updated)
+    def update(self, task_id: int, task_data: TaskUpdateDTO):
+        logging.info(f'Atualizando a tarefa {task_id}.')
+        task = self._read(task_id)
+        task_data = task_data.model_dump(exclude_unset=True)
+        for key, value in task_data.items():
+            setattr(task, key, value)
+        task_updated = self.task_repository.save(task)
+        logging.info(f'Tarefa {task_id} atualizado: {task_updated}')
+        return TypeAdapter(TaskDTO).validate_python(task_updated)
 
-    def delete(self, user_id: int) -> int:
-        user = self._read(user_id)
-        self.task_repository.delete(user)
-        logging.info(f'Tarefa {user_id} deletado')
-        return user_id
+    def delete(self, task_id: int) -> int:
+        task = self._read(task_id)
+        self.task_repository.delete(task)
+        logging.info(f'Tarefa {task_id} deletado')
+        return task_id
